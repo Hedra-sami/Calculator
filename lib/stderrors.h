0bab4dff2332472555b10a6b6e8def8609a90ad8
Hedra-sami <61567779+Hedra-sami@users.noreply.github.com>
2023-10-26 19:05:49 +0200

    Calculator using keybad and lcd

---
+++ lib/stderrors.h
@@ -0,0 +1,15 @@
+/*
+ * stderrors.h
+ *
+ *  Created on: Aug 12, 2023
+ *      Author: hedra
+ */
+
+#ifndef LIB_STDERRORS_H_
+#define LIB_STDERRORS_H_
+
+#define RT_OK (u8)0
+#define RT_NOK (u8)1
+
+
+#endif /* LIB_STDERRORS_H_ */
---
+++ lib/stdtypes.h
@@ -0,0 +1,20 @@
+/*
+ * stdtypes.h
+ *
+ *  Created on: Aug 12, 2023
+ *      Author: hedra
+ */
+
+#ifndef LIB_STDTYPES_H_
+#define LIB_STDTYPES_H_
+
+typedef unsigned char u8;
+typedef signed char s8;
+typedef unsigned short int u16;
+typedef signed short int s16;
+typedef unsigned long int u32;
+typedef signed long int s32;
+typedef float f32;
+typedef double f64;
+
+#endif /* LIB_STDTYPES_H_ */
